01a1c15bc70def0f0afba6f2ef0f6b9ed6a0c696
david <kdavidmacharia8485@gmail.com>
2025-03-07 10:31:42 +0300

    render api

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -23,12 +23,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^648lrb1z88_@vi#fyz%-fxsxjfg3o2+)%ftv3^v=uh2&cydky"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cinetix-api.onrender.com', '127.0.0.1', 'localhost']
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Allow frontend to access API
-    # "https://carparkingsystem.vercel.app",  
+   'http://localhost:5173',
+   'https://cine-tix-iota.vercel.app',
+   'https://cinetix-api.onrender.com',
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+    'accept',
 ]
 
 
