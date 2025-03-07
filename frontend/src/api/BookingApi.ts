b31c7b0303c2e4e739b305d82a9b2e4e55c6912b
david <kdavidmacharia8485@gmail.com>
2025-03-07 10:32:13 +0300

    online api

--- frontend/src/api/BookingApi.ts
+++ frontend/src/api/BookingApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/booking/';
+const API_URL = 'https://cinetix-api.onrender.com/api/booking/';
 
 export interface Booking {
     booking_id?: number;
